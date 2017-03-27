602ef040fe274aa988f904386c7007bc57170f88
Joseph Malachosky <Joseph.Malachosky@stu.laroche.edu>
2017-03-26 20:39:41 -0400

    First commit

---
+++ app/src/main/java/com/projects/malachosky/perulrc/activities/NotesContainerActivity.java
@@ -0,0 +1,20 @@
+package com.projects.malachosky.perulrc.activities;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.projects.malachosky.perulrc.R;
+
+public class NotesContainerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes_container);
+
+    }
+
+
+
+}
