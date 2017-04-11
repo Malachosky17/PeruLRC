111f417a2237c57e6438a46f7c55fa73d54d3319
Joseph Malachosky <Joseph.Malachosky@stu.laroche.edu>
2017-04-11 16:22:10 -0400

    Removing the inner class NotesContainerActivity.NotesAdapter and moved it into its own class ..\adapters\NotesAdapter.java

---
+++ app/src/main/java/com/projects/malachosky/perulrc/adapters/NotesAdapter.java
@@ -0,0 +1,12 @@
+package com.projects.malachosky.perulrc.adapters;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by Malac on 4/11/2017.
+ *
+ * @author: Malac
+ */
+
+public class NotesAdapter extends RecyclerView.Adapter {
+}
